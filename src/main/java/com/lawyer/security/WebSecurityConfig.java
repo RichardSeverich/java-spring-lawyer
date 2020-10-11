792e06ad4647d58f4da443086ca354b004688c4c
charly <charlymeneses7@gmail.com>
2020-10-11 14:30:55 -0400

    add swagger

---
+++ src/main/java/com/lawyer/security/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.lawyer.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private static final String[] AUTH_WHITELIST = {
+    // -- swagger ui
+    "/v2/api-docs",
+    "/swagger-resources",
+    "/swagger-resources/**",
+    "/configuration/ui",
+    "/configuration/security",
+    "/swagger-ui.html",
+    "/webjars/**"
+    // other public endpoints of your API may be appended to this array
+  };
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+      .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+      .authorizeRequests()
+      .antMatchers(HttpMethod.POST, "/api/v1/login").permitAll()
+      .antMatchers(AUTH_WHITELIST).permitAll()
+      .anyRequest().authenticated();
+  }
+}
